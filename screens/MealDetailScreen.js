978ca3ddd2dbf9138bf80912155342c028aca364
Alexander <alexander.fau.usa@gmail.com>
2021-04-30 14:12:17 -0400

    created initail file structure, with the different screens, and packages

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, text, StyleSheet } from "react-native";
+
+const MealDetailScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Meal Detail Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MealDetailScreen;
